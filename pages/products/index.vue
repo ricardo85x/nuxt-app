299df0e034c31781684b67c31cee34abedcafdf7
ricardo <ricardo85x@gmail.com>
2022-05-01 11:31:44 -0300

    feat: data fetching with fetch, module and if else

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -3,7 +3,10 @@
     <h1 class="text-lg font-bold">Products</h1>
     <NuxtLink to="/products/product-a"> Product A</NuxtLink>
 
-    <ul style="list-style-type: circle;" v-for="product in products" :key="product.id">
+    <ul 
+      style="list-style-type: circle;" 
+      v-for="product in products" :key="product.id"
+    >
       <li>{{ product.title }}</li>
     </ul>
     
@@ -17,6 +20,7 @@ export default {
   data() {
     return {};
   },
+  // before the component is created
   async asyncData({$axios}) {
     
     const products = await $axios.$get("https://jsonplaceholder.typicode.com/posts?_limit=3")
--- pages/services.vue
+++ pages/services.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
-    <h1 class="text-lg font-bold">Services</h1>
 
-    <NuxtLink to="/services/website-development">WebSite development</NuxtLink>
-    <NuxtLink to="/services/marketing-digital">Marketing digital</NuxtLink>
+    <div v-if="$fetchState.pending">
+      Loading...
+    </div>
 
+    <div v-else>
+ <h1 class="text-lg font-bold">Services: {{ service_name }}</h1>
+
+   
+    
+    
+
+    <ul style="list-style-type: circle" v-for="service in services" :key="service.id">
+        <li>
+          <NuxtLink :to="`/services/${service.name}`">{{service.name}}</NuxtLink>
+        </li>
+    </ul>
+
+    </div>
+   
+
+ 
     <div class="pt-4">
       <NuxtChild />
     </div>
@@ -12,8 +29,27 @@
 </template>
 
 <script>
+import { faker } from "@faker-js/faker"
 export default {
   name: "services",
+
+  data() {
+    return {
+      services: [],
+      service_name: ""
+    }
+  },
+
+  // after the component is created
+  async fetch() {
+    
+    
+    this.services = await this.$axios.$get("https://jsonplaceholder.typicode.com/users?_limit=3")
+
+    this.service_name = faker.animal.dog()
+
+    //console.log(this)
+  }
   
 
 };
