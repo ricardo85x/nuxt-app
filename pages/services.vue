9dfb16da1acc450d052f5005cee0524f1b7aa306
ricardo <ricardo85x@gmail.com>
2022-05-01 11:59:41 -0300

    feat: SEO, created middleware and methods

--- pages/services.vue
+++ pages/services.vue
@@ -1,27 +1,26 @@
 <template>
   <div>
 
-    <div v-if="$fetchState.pending">
-      Loading...
-    </div>
+    title: {{ title }}
+
+    <div v-if="$fetchState.pending">Loading...</div>
 
     <div v-else>
- <h1 class="text-lg font-bold">Services: {{ service_name }}</h1>
+      <h1 class="text-lg font-bold">Services: {{ service_name }}</h1>
 
-   
-    
-    
-
-    <ul style="list-style-type: circle" v-for="service in services" :key="service.id">
+      <ul
+        style="list-style-type: circle"
+        v-for="service in services"
+        :key="service.id"
+      >
         <li>
-          <NuxtLink :to="`/services/${service.name}`">{{service.name}}</NuxtLink>
+          <NuxtLink :to="`/services/${service.name}`">{{
+            service.name
+          }}</NuxtLink>
         </li>
-    </ul>
-
+      </ul>
     </div>
-   
 
- 
     <div class="pt-4">
       <NuxtChild />
     </div>
@@ -29,28 +28,51 @@
 </template>
 
 <script>
-import { faker } from "@faker-js/faker"
+import { faker } from "@faker-js/faker";
 export default {
   name: "services",
+
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: 'Services Page' },
+      ],
+      bodyAttrs: {
+        class: "bg-gray-300"
+      }
+    }
+    
+  },
 
   data() {
     return {
       services: [],
-      service_name: ""
-    }
+      service_name: "",
+      title: ""
+    };
   },
 
   // after the component is created
   async fetch() {
-    
-    
-    this.services = await this.$axios.$get("https://jsonplaceholder.typicode.com/users?_limit=3")
+    this.services = await this.$axios.$get(
+      "https://jsonplaceholder.typicode.com/users?_limit=3"
+    );
 
-    this.service_name = faker.animal.dog()
+    this.service_name = faker.animal.dog();
 
     //console.log(this)
-  }
-  
+  },
 
+  created() {
+    this.getTitle()
+  },
+
+
+  methods: {
+    getTitle() {
+      setTimeout(() => this.title = "Ŝervice", 3000 )
+    }
+  }
 };
 </script>
